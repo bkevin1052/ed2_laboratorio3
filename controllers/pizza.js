d7b06526f38e4004d0583e15ed5228172ee217a0
LeslyDubon <35675890+LeslyDubon@users.noreply.github.com>
2018-11-14 12:39:33 -0600

    Validaciones_pizza

--- controllers/pizza.js
+++ controllers/pizza.js
@@ -1,4 +1,6 @@
 var Pizza = require('../models/pizza');
+var HttpStatus = require('http-status-codes');
+var tryparse = require('tryparse');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
@@ -6,14 +8,62 @@ exports.test = function (req, res) {
 };
 
 exports.pizza_create = function (req, res) {
-    var pizza = new Pizza();        
+    var pizza = new Pizza(); 
+
         pizza.nombre = req.body.nombre;
+        if(pizza.nombre ==null)
+        {
+            res.send('Falta el nombre')
+            return;
+        }
+
         pizza.descripcion = req.body.descripcion;
+        if(pizza.descripcion ==null)
+        {
+            res.send('Falta la descripción')
+            return;
+        }      
+
         pizza.ingredientes = req.body.ingredientes;
+        if(pizza.ingredientes ==null)
+        {
+            res.send('Faltan los ingredientes')
+            return;
+        }
+
         pizza.tipoMasa = req.body.tipoMasa;
+        if(pizza.tipoMasa ==null)
+        {
+            res.send('Falta el tipo de masa')
+            return;
+        }
+
         pizza.tamanio = req.body.tamanio;
+        if(pizza.tamanio ==null)
+        {
+            res.send('Falta el tamaño')
+            return;
+        }
+
         pizza.cantidadPorciones = req.body.cantidadPorciones;
+        if(pizza.cantidadPorciones ==null)
+        {
+            res.send('Falta la cantidad de porciones')
+            return;
+        }
+        else if(tryparse.int(pizza.cantidadPorciones) == null)
+        {
+            res.send('No es un número de porciones válido')
+            return;
+        }
+
         pizza.quesoExtra = req.body.quesoExtra; 
+        if(pizza.quesoExtra ==null)
+        {
+            res.send('Por favor, indique si quiere queso extra')
+            return;
+        }
+
     pizza.save(function (err) {
         if (err) {
             return next(err);
