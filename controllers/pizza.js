6d24e7b6a13988da854b198371d51b408eddc1f4
LeslyDubon <35675890+LeslyDubon@users.noreply.github.com>
2018-11-14 14:15:13 -0600

    Validaciones_en_update

--- controllers/pizza.js
+++ controllers/pizza.js
@@ -79,7 +79,63 @@ exports.pizza_details = function (req, res) {
     });
 };
 
-exports.pizza_update = function (req, res) {
+exports.pizza_update = function (req, res) {   
+    var pizza = new Pizza(); 
+
+    pizza.nombre = req.body.nombre;
+    if(pizza.nombre ==null)
+    {
+        res.send('Falta el nombre')
+        return;
+    }
+
+    pizza.descripcion = req.body.descripcion;
+    if(pizza.descripcion ==null)
+    {
+        res.send('Falta la descripción')
+        return;
+    }      
+
+    pizza.ingredientes = req.body.ingredientes;
+    if(pizza.ingredientes ==null)
+    {
+        res.send('Faltan los ingredientes')
+        return;
+    }
+
+    pizza.tipoMasa = req.body.tipoMasa;
+    if(pizza.tipoMasa ==null)
+    {
+        res.send('Falta el tipo de masa')
+        return;
+    }
+
+    pizza.tamanio = req.body.tamanio;
+    if(pizza.tamanio ==null)
+    {
+        res.send('Falta el tamaño')
+        return;
+    }
+
+    pizza.cantidadPorciones = req.body.cantidadPorciones;
+    if(pizza.cantidadPorciones ==null)
+    {
+        res.send('Falta la cantidad de porciones')
+        return;
+    }
+    else if(tryparse.int(pizza.cantidadPorciones) == null)
+    {
+        res.send('No es un número de porciones válido')
+        return;
+    }
+
+    pizza.quesoExtra = req.body.quesoExtra; 
+    if(pizza.quesoExtra ==null)
+    {
+        res.send('Por favor, indique si quiere queso extra')
+        return;
+    }
+
     Pizza.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, pizza) {
         if (err) return next(err);
         res.send('Pizza Actualizada');
